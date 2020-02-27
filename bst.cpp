3dd4928b8e2d77c3c9418288231274f5567a2fd1
ista2000 <mishra.istasis@gmail.com>
2020-02-27 22:17:06 +0000

    Added remove function

--- bst.cpp
+++ bst.cpp
@@ -13,12 +13,17 @@ class BST {
 public:
 	BSTNode nodes[52];
 	int free_head, free_tail, sz;
-	BST() {
+	BST() 
+	{
 		free_head = 0;
 		free_tail = 51;
 		sz = 0;
 		for(int i = 0;i<51;i++)
-			nodes[i].data = i+1,nodes[i].left = -1, nodes[i].right = -1;
+		{
+			nodes[i].data = i+1;
+			nodes[i].left = -1;
+			nodes[i].right = -1;
+		}
 	}
 	// int inorder(int cur,int &N) // Have to implement properly
 	// {
@@ -34,7 +39,8 @@ public:
 	// 	if(nodes[cur].right!=-1)
 	// 		N = inorder(nodes[cur].right, N);
 	// }
-	bool insert(int c, int data, int ends) {
+	bool insert(int c, int data, int ends) 
+	{
 		int cur = 0;
 		while(1) {
 			if(!sz)
@@ -65,34 +71,161 @@ public:
 		sz++;
 		return true;
 	}
-	int find(int c) {
+	int find(int c) 
+	{
+		if(!sz) return -1;
 		int cur = 0;
-		while(cur != -1) {
+		while(cur != -1) 
+		{
 			if(nodes[cur].c == c)
-				return nodes[cur].data;
+				return cur;
 			if(nodes[cur].c < c)
 				cur = nodes[cur].right;
 			else
 				cur = nodes[cur].left;
 		}
-		return 0;
+		return -1;
 	}
-	bool remove(int c) // Have to implement properly
+	int par(int c)
 	{
-		int cur = 0;
-		while(nodes[cur].left != nodes[cur].right) {
-			if(nodes[cur].c == c) {
-				nodes[cur].left = nodes[cur].right = -1;
-				nodes[free_tail].data = cur;
-				free_tail = cur;
+		if(!sz) return -1;
+		int cur = 0, par = -1;
+		while(cur != -1) 
+		{
+			if(nodes[cur].c == c)
+				return par;
+			par = cur;
+			if(nodes[cur].c < c)
+				cur = nodes[cur].right;
+			else
+				cur = nodes[cur].left;
+		}
+		return -1;
+	}
+	bool remove(int c)
+	{
+		if(!sz) return false;
+		if(sz == 1)
+		{
+			sz = 0;
+			nodes[0].left = -1;
+			nodes[0].right = -1;
+			nodes[0].data = free_head;
+			free_head = 0;
+		}
+		int cur = find(c);
+		if(cur == -1)
+			return false;
+
+		if(nodes[cur].left == -1 && nodes[cur].right == -1)
+		{
+			int p = par(c);
+			nodes[cur].data = 0;
+			nodes[free_tail].data = cur;
+			free_tail = cur;
+			if(nodes[p].c < c)
+				nodes[p].right = -1;
+			else
+				nodes[p].left = -1;
+			sz--;
+			return true;
+		}
+
+		while(nodes[cur].left != -1 && nodes[cur].right != -1)
+		{
+			int ncur = nodes[cur].left;
+			nodes[cur].data = nodes[ncur].data;
+			nodes[cur].ends = nodes[ncur].data;
+			nodes[cur].c = nodes[ncur].c;
+			if(nodes[ncur].left == -1 && nodes[ncur].right == -1)
+			{
+				nodes[ncur].data = 0;
+				nodes[free_tail].data = ncur;
+				free_tail = ncur;
+				nodes[cur].left = -1;
 				sz--;
 				return true;
 			}
-			if(nodes[cur].c < c)
-				cur = nodes[cur].right;
-			else
-				cur = nodes[cur].left;
+			cur = ncur;
 		}
-		return false;
+		if(nodes[cur].left == -1)
+		{
+			int ncur =  nodes[cur].right;
+			nodes[cur].left = nodes[ncur].left;
+			nodes[cur].right = nodes[ncur].right;
+			nodes[cur].c = nodes[ncur].c;
+			nodes[cur].data = nodes[ncur].data;
+			nodes[cur].ends = nodes[ncur].ends;
+			nodes[ncur].left = -1;
+			nodes[ncur].right = -1;
+			nodes[ncur].data = 0;
+			nodes[free_tail].data = ncur;
+			free_tail = ncur;
+		}
+		else
+		{
+			int ncur =  nodes[cur].left;
+			nodes[cur].left = nodes[ncur].left;
+			nodes[cur].right = nodes[ncur].right;
+			nodes[cur].c = nodes[ncur].c;
+			nodes[cur].data = nodes[ncur].data;
+			nodes[cur].ends = nodes[ncur].ends;
+			nodes[ncur].left = -1;
+			nodes[ncur].right = -1;
+			nodes[ncur].data = 0;
+			nodes[free_tail].data = ncur;
+			free_tail = ncur;
+		}
+		sz--;
+		return true;
 	}
 };
+
+int main()
+{
+	BST bst;
+	bst.insert(0, 5, 0);
+	bst.insert(5, 0, 0);
+	bst.insert(2, 3, 0);
+	bst.insert(4, 1, 0);
+	bst.insert(1, 4, 0);
+	bst.insert(3, 2, 0);
+	for(int i = 0;i<6;i++)
+		cout<<bst.find(i)<<" ";
+	cout<<endl;
+	for(int i = 0;i<6;i++)
+	{
+		bst.remove(i);
+		for(int j = 0;j<6;j++)
+		{
+			cout<<bst.find(j)<<" ";
+			if(bst.find(j)!=-1)
+				cout<<bst.nodes[bst.find(j)].data;
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+
+
+	bst.insert(0, 5, 0);
+	bst.insert(5, 0, 0);
+	bst.insert(2, 3, 0);
+	bst.insert(4, 1, 0);
+	bst.insert(1, 4, 0);
+	bst.insert(3, 2, 0);
+	for(int i = 0;i<6;i++)
+		cout<<bst.find(i)<<" ";
+	cout<<endl;
+	for(int i = 0;i<6;i++)
+	{
+		bst.remove(i);
+		for(int j = 0;j<6;j++)
+		{
+			cout<<bst.find(j)<<" ";
+			if(bst.find(j)!=-1)
+				cout<<bst.nodes[bst.find(j)].data;
+			cout<<endl;
+		}
+		cout<<endl;
+	}
+}
